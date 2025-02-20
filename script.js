b4b7f465d62f0e331b05accc6b58a6d9c7118b68
arthur augusto hencke berti <arthurhencke@gmail.com>
2025-02-20 19:33:54 -0300

    codigo pronto

---
+++ script.js
@@ -0,0 +1,38 @@
+const input = document.querySelector("#novotext");
+const button = document.querySelector("form button");
+const teste =  document.querySelector("h2");
+const ul = document.querySelector("ul");
+button.onclick = ev => {
+    ev.preventDefault();
+    if(input.value){
+        const span = document.createElement("span");
+        span.innerHTML = input.value;
+        span.setAttribute("class", "tarefa")
+
+        const libutton = document.createElement("button");
+        libutton.innerHTML = `Concluida`;
+        libutton.setAttribute("class", "libutton")
+
+        const deletar = document.createElement("button");
+        deletar.innerHTML = `Deletar`
+        deletar.setAttribute("class", "deletar")
+
+        const liitem = document.createElement("li");
+        liitem.appendChild(span)
+        liitem.appendChild(libutton)
+        liitem.appendChild(deletar)
+
+        libutton.onclick = () =>{
+            // ul.removeChild(liitem)
+            liitem.setAttribute('style', "text-decoration: line-through;")
+        }
+
+        deletar.onclick = () =>{
+            ul.removeChild(liitem)
+        }
+
+        ul.appendChild(liitem );
+        input.value = "";
+    }
+}
+
